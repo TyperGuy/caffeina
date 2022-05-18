731bd4d5becc7221d35148af13b39cd5f7f76a67
TyperGuy <ray.developper@gmail.com>
2022-05-18 18:12:02 -0400

    Update node Version

--- src/components/landing-page-float-buttons/landing-page-float-buttons.styles.ts
+++ src/components/landing-page-float-buttons/landing-page-float-buttons.styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
   justify-content: space-evenly;
   position: fixed;
   z-index: 20000;
-  right: 100px;
+  right: 20px;
   bottom: 20px;
   box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
   border-radius: 50%;
